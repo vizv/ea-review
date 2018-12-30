184822839e7b4eaa5b4b588d5ab0b5935f508a8f
Wenxuan Zhao <viz@linux.com>
2018-12-29 23:43:15 -0800

    [API] Move models and controllers to app directory, add Rakefile back

--- api/config/application.rb
+++ api/config/application.rb
@@ -6,7 +6,7 @@ require_relative 'db_init'
 puts "Loading components..."
 %w(model controller).each do |component|
   puts "  Loading #{component}..."
-  Dir[File.join("#{component}s", "**/*.rb")].each do |file|
+  Dir[File.join("app", "#{component}s", "**/*.rb")].each do |file|
     require File.expand_path(file)
     puts "    Load #{component} #{File.basename file}"
   end
